41d61873a293ad16ccffe783d6739f8ae77d908e
Lukáš Veškrna <lukas.veskrna@gmail.com>
2025-03-06 16:23:37 +0100

    Fixed add another button, admin improvements

--- scents/admin.py
+++ scents/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from django.contrib.gis.admin import GISModelAdmin
 from . import models
 
 
-admin.site.register(models.Scent, admin.ModelAdmin)
+class AttachmentInline(admin.TabularInline):
+	model = models.Attachment
+
+
+class ScentAdmin(GISModelAdmin):
+	inlines = [AttachmentInline]
+
+admin.site.register(models.Scent, ScentAdmin)
